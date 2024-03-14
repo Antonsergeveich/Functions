55903e184f2289ccf7f2a103d389075457baeae2
Persh_vx33@top-academy.ru <Persh_vx33@top-academy.ru>
2024-03-14 16:09:44 +0300

    Functions

--- Arrays/main.cpp
+++ Arrays/main.cpp
@@ -1,13 +1,23 @@
 ﻿#include<iostream>
+#include<Windows.h>
 using namespace std;
 
+void FillRand(int arr[], const int n);
+void Print(int arr[], const int n);
+int Sum(int arr[], const int n);
+double Avg(int arr[], const int n);
+int minValueIn(int arr[], const int n);
+int maxValueIn(int arr[], const int n);
+void shiftLeft(int arr[], const int n);
+void shiftRight(int arr[], const int n);
 #define tab "\t"
-
+//#define FillRand
 void main()
 {
 	setlocale(LC_ALL, "");
 	const int n = 5;
 	int arr[n];
+#ifdef FillRand
 	int minRand, maxRand;
 	do
 	{
@@ -37,4 +47,105 @@ void main()
 		cout << arr[i] << tab;
 	}
 	cout << endl;
+#endif // FillRand
+	FillRand(arr, n); // Заполняем массив случайными числами
+	cout << "Выводим массив на экран: "; Print(arr, n);
+	cout << endl;
+	cout << "Возвращаем сумма всех элементов массива: " << Sum(arr, n) << endl;
+	cout << "Возвращаем среднее арифметическое элементов массива: " << Avg(arr, n) << endl;
+	cout << "Возвращаем минимальное значение в массиве: " << minValueIn(arr, n) << endl;
+	cout << "Возвращаем максимальное значение в массиве: " << maxValueIn(arr, n) << endl;
+	cout << "Циклически сдвигаем массив на заданное число элементов влево :" << endl;
+    shiftLeft(arr, n);
+	cout << "Циклически сдвигаем массив на заданное число элементов вправо :" << endl;
+	shiftRight(arr, n);
+}
+ 
+void FillRand(int arr[], const int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		arr[i] = rand();
+	}
+}
+void Print(int arr[], const int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		cout << arr[i] << "\t";
+	}
+}
+int Sum(int arr[], const int n)
+{
+	int sum = 0;
+	for (int i = 0; i < n; i++)
+	{
+		sum = sum + arr[i];
+	}
+	return(sum);
+}
+double Avg(int arr[], const int n)
+{
+	double s = 0;
+	double sa;
+	for (int i = 0; i < n; i++)
+	{
+		s = s + arr[i];
+	}
+	sa = s / n;
+	return(sa);
+}
+int minValueIn(int arr[], const int n)
+{
+	int min;
+	min = arr[0];
+	for (int i = 0; i < n; i++)
+	{
+		if (arr[i] < min)min = arr[i];
+	}
+	return (min);
+}
+int maxValueIn(int arr[], const int n)
+{
+	int max;
+    max = arr[0];
+	for (int i = 0; i < n; i++)
+	{
+		if (arr[i] > max)max = arr[i];
+	}
+	return (max);
+}
+void shiftLeft(int arr[], const int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		int buffer = arr[0];
+		for (int i = 0; i < n; i++)
+		{
+			arr[i] = arr[i + 1];
+		}
+		arr[n - 1] = buffer;
+		for (int i = 0; i < n; i++)
+		{
+			cout << arr[i] << "\t";
+		}
+		cout << endl;
+	}
+}
+void shiftRight(int arr[], const int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		int buffer = arr[n-1];
+		for (int i = n-2; i >= 0; i--)
+		{
+			arr[i+1] = arr[i];
+		}
+		arr[0] = buffer;
+		for (int i = 0; i < n; i++)
+		{
+			cout << arr[i] << "\t";
+		}
+		cout << endl;
+	}
 }
