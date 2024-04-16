d996ecba839016775a5904106a46898a07745fb2
Persh_vx33@top-academy.ru <Persh_vx33@top-academy.ru>
2024-04-16 16:31:31 +0300

    Arrays

--- Arrays/main.cpp
+++ Arrays/main.cpp
@@ -20,7 +20,7 @@ void FillRand(double arr[ROWS][COLS], const int ROWS, const int COLS, int minRan
 void FillRand(char arr[ROWS][COLS], const int ROWS, const int COLS, int minRand = 0, int maxRand = 100);
 void Unique(int arr[], const int n, int minRand = 0, int maxRand = 100);
 void Unique_1(int arr[], const int n);
-void Unique_2(int arr[ROWS][COLS], int ROWS, int COLS);
+void Unique_2(int arr[ROWS][COLS], const int ROWS, const int COLS);
 
 void Print(const int arr[], const int n);
 void Print(const double arr[], const int n);
@@ -311,7 +311,7 @@ void Unique_1(int arr[], const int n)
 		} while (!unique);
 	}
 }
-void Unique_2(int arr[ROWS][COLS],  int ROWS,  int COLS)
+void Unique_2(int arr[ROWS][COLS], const int ROWS,  const int COLS)
 {
 	for (int i = 0; i < ROWS; i++)
 	{
